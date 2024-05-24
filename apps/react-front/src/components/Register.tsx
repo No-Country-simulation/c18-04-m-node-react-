53dfbbd2889bd5bbd2e6601ad860ffa7c089203d
Emanuel Pesci <emanuelpesci@gmail.com>
2024-05-24 09:51:26 -0300

    feat: Agregue los campos nombre, apellido y telefono

--- apps/react-front/src/components/Register.tsx
+++ apps/react-front/src/components/Register.tsx
@@ -4,11 +4,17 @@ import { Link } from 'react-router-dom';
 export default function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phone, setPhone] = useState('');
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     
     const result = {
+      firstName,
+      lastName,
+      phone,
       email,
       password
     }
@@ -22,6 +28,39 @@ export default function Register() {
 
         <form onSubmit={handleSubmit} className="mt-6">
           <div>
+            <label className="block text-sm text-gray-800">Nombre</label>
+            <input
+              type="text"
+              className="w-full px-4 py-2 mt-2 text-gray-700 bg-white rounded-lg focus:ring-2 focus:ring-blue-600 focus:outline-none"
+              placeholder="Nombre"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+
+          <div className="mt-4">
+            <label className="block text-sm text-gray-800">Apellido</label>
+            <input
+              type="text"
+              className="w-full px-4 py-2 mt-2 text-gray-700 bg-white rounded-lg focus:ring-2 focus:ring-blue-600 focus:outline-none"
+              placeholder="Apellido"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+
+          <div className="mt-4">
+            <label className="block text-sm text-gray-800">Teléfono</label>
+            <input
+              type="tel"
+              className="w-full px-4 py-2 mt-2 text-gray-700 bg-white rounded-lg focus:ring-2 focus:ring-blue-600 focus:outline-none"
+              placeholder="Teléfono"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+
+          <div className="mt-4">
             <label className="block text-sm text-gray-800">Email</label>
             <input
               type="email"
@@ -56,4 +95,4 @@ export default function Register() {
       </div>
     </div>
   );
-} 
+}

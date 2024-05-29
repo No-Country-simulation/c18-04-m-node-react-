635c5828a70fc2f3359516aaf47a7b6282229b77
Zanchez <104705472+Z4nchez@users.noreply.github.com>
2024-05-29 13:27:57 -0300

    fix: update tailwind.config.ts #86a3gu3ye

--- apps/react-front/tailwind.config.js
+++ apps/react-front/tailwind.config.js
@@ -10,23 +10,19 @@ module.exports = {
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  theme: {
-    //Config Global Colors
-    colors:{
-      'yellow' : 'rgba(214, 186, 0, 1)',
-      'darkPurple' : 'rgba(18, 9, 26, 1)',
-      'lightPurple': 'rgba(164, 155, 155, 0.14)',
-      'grey' : 'rgba(164, 155, 155, 1)'
-    },
-    //Config Screens
-    screens:{
-      'sm': '393',
-      'xl': '1920'
-    },
-    fontFamily:{
-      'marcellus':["Marcellus SC", "sans-serif"],
-    },
+  theme: {    
     extend:{
+      //Config Global Colors
+      colors:{
+        'yellow' : 'rgba(214, 186, 0, 1)',
+        'darkPurple' : 'rgba(18, 9, 26, 1)',
+        'lightPurple': 'rgba(164, 155, 155, 0.14)',
+        'grey' : 'rgba(164, 155, 155, 1)',
+        'aura' : 'rgba(0, 0, 0, 0.3)'
+      },
+      fontFamily:{
+        'marce':["Marcellus SC", "sans-serif"],
+      },
     },
   },
   plugins: [],

f9a09f88026230a105830155fbbb42b13c6d08c9
dqniel12396 <danielsanabria12396@gmail.com>
2024-05-29 20:41:35 -0500

    feat : creacion de homepage con react #86a3gu32a

--- apps/react-front/src/app/app.tsx
+++ apps/react-front/src/app/app.tsx
@@ -1,9 +1,9 @@
 import {FrontComponents} from "@c18-04-m-node-react/front-components"
-
+import Home from "../pages/Home";
 export function App() {
   return (
     <div>
-      <FrontComponents/>
+      <Home/>
     </div>
   );
 }
--- apps/react-front/src/components/Header.tsx
+++ apps/react-front/src/components/Header.tsx
@@ -1,6 +1,18 @@
+import React from 'react';
 
-export default function Header () {
-  return (
-    <div>Header</div>
-  )
-}
+const Header = () => {
+    return (
+        <header className="flex items-center justify-between p-4 bg-black text-yellow-500">
+            <div className="flex items-center">
+                <img src="/src/images_Home/logo_legal_builder.svg" alt="Legal Builder Logo" className="h-10 w-10" />
+                <h1 className="ml-4 text-2xl">Legal Builder</h1>
+            </div>
+            <div className="flex space-x-4">
+                <a href="#" className="hover:underline">Login</a>
+                <a href="#" className="hover:underline">Register</a>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
